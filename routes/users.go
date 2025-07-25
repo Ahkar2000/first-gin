43c7cbb9bde65f2c13a76cd3510295bd97741df1
Ahkar2000 <ahkarminhtut111@gmail.com>
2025-07-25 09:32:06 +0630

    first

---
+++ routes/users.go
@@ -0,0 +1,41 @@
+package routes
+
+import (
+	"first-gin/models"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message" : "Validation wrong."})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message" : "Could not save user."})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message" : "User created!", "user" : user})
+}
+
+func login(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+	if err != nil{
+		context.JSON(http.StatusBadRequest, gin.H{"message" : "Validation wrong."})
+		return
+	}
+
+	err = user.ValidateUser()
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message" : err.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message" : "Login success."})
+}
